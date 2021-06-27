53dcbd297a2edffd26c6d316467266ac180d4660
ctykwz <ctykwz@gmail.com>
2021-06-27 20:10:35 +0800

    括号生成

--- src/main/java/com/icu/ctyk/num/GenerateParenthesis.java
+++ src/main/java/com/icu/ctyk/num/GenerateParenthesis.java
@@ -1,5 +1,6 @@
 package com.icu.ctyk.num;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,7 +8,35 @@ import java.util.List;
  */
 public class GenerateParenthesis {
 
-    public List<String> generateParenthesis(int n) {
+    public static void main(String[] args) {
+        System.out.println(new GenerateParenthesis().generateParenthesis(3));
+    }
 
+    public List<String> generateParenthesis(int n) {
+        if (n <= 0) {
+            return new ArrayList<>();
+        }
+        List<String> res = new ArrayList<>();
+        dfs(res, new StringBuilder(), n, n);
+        return res;
+    }
+
+    private void dfs(List<String> res, StringBuilder builder, int left, int right) {
+        if (left == 0 && right == 0) {
+            res.add(builder.toString());
+            return;
+        }
+        // 括号合法的条件是右括号数小于左括号
+        if (left > right) {
+            return;
+        }
+        if (left > 0) {
+            dfs(res, builder.append("("), left - 1, right);
+            builder.deleteCharAt(builder.length() - 1);
+        }
+        if (right > 0) {
+            dfs(res, builder.append(")"), left, right - 1);
+            builder.deleteCharAt(builder.length() - 1);
+        }
     }
 }
