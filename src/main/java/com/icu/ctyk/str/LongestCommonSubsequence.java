b4e6a720bd4bbee1a30421fc6bcf3f339c9025fe
ctykwz <ctykwz@gmail.com>
2021-06-28 11:10:58 +0800

    fix

--- src/main/java/com/icu/ctyk/str/LongestCommonSubsequence.java
+++ src/main/java/com/icu/ctyk/str/LongestCommonSubsequence.java
@@ -5,14 +5,22 @@ package com.icu.ctyk.str;
  */
 public class LongestCommonSubsequence {
 
+    public static void main(String[] args) {
+        System.out.println(new LongestCommonSubsequence().longestCommonSubsequence(
+                "abcde",
+                "ace"
+        ));
+    }
+
     public int longestCommonSubsequence(String text1, String text2) {
         if (text1 == null || text2 == null) {
             return 0;
         }
         // 从text1的前i个字符到text2的前j个字符 最长公共子串
         int[][] dp = new int[text1.length() + 1][text2.length() + 1];
-        for (int i = 1; i < text1.length(); i++) {
-            for (int j = 1; j < text2.length(); j++) {
+        for (int i = 1; i <= text1.length(); ++i) {
+            for (int j = 1; j <= text2.length(); ++j) {
+                // 为了方便  这里没有从0开始，取i-1字符和j-1字符作为 dp[i][j]
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
