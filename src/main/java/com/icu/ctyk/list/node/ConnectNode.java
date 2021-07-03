9780d03d00a6b96d3aa1c76317be2b1c524acc3c
ctykwz <ctykwz@gmail.com>
2021-07-03 22:12:19 +0800

    完美二叉树next指针

---
+++ src/main/java/com/icu/ctyk/list/node/ConnectNode.java
@@ -0,0 +1,57 @@
+package com.icu.ctyk.list.node;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author xiyun on 2021-07-03 22:00
+ */
+public class ConnectNode {
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                if (i < size - 1) {
+                    node.next = queue.peek();
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return root;
+    }
+
+    static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    ;
+}
