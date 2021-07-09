e162e54567083d714a17f90dfbf9bc8280e7c5ab
ctykwz <ctykwz@gmail.com>
2021-07-09 10:28:16 +0800

    摩尔投票法

--- src/main/java/com/icu/ctyk/array/MajorityElement.java
+++ src/main/java/com/icu/ctyk/array/MajorityElement.java
@@ -1,6 +1,8 @@
 package com.icu.ctyk.array;
 
 /**
+ * 摩尔投票法本事默认要求存在众数 不存在的话是要自己验证下的
+ *
  * @author ctykwz on 2021-06-19 22:41
  */
 public class MajorityElement {
@@ -15,5 +17,27 @@ public class MajorityElement {
             cnt += candidate == num ? 1 : -1;
         }
         return candidate;
+    }
+
+
+    /**
+     * 默认不一定存在众数时
+     */
+    public int majorityElementV2(int[] nums) {
+        int cnt = 0;
+        int flag = -1;
+        for (int num : nums) {
+            if (cnt == 0) {
+                flag = num;
+            }
+            cnt = flag == num ? cnt + 1 : cnt - 1;
+        }
+        cnt = 0;
+        for (int num : nums) {
+            if (flag == num) {
+                cnt += 1;
+            }
+        }
+        return cnt > nums.length / 2 ? flag : -1;
     }
 }
